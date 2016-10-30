0cd7d1b9aba14c5249315ef215e9c082df9b82e0
Pete Jeffryes <pete@flowhub.co>
2016-10-30 15:42:27 -0600

    finished tut

--- features/steps/auth_steps.py
+++ features/steps/auth_steps.py
@@ -4,6 +4,10 @@ from behave import *
 def flask_is_set_up(context):
     assert context.client
 
+@given(u'we are not logged in')
+def logout(context):
+    context.page = context.client.get('/logout', follow_redirects=True)
+
 @given(u'we log in with "{username}" and "{password}"')
 @when(u'we log in with "{username}" and "{password}"')
 def login(context, username, password):
@@ -22,3 +26,12 @@ def logout(context):
 @then(u'we should see the alert "{message}"')
 def message(context, message):
     assert str.encode(message) in context.page.data
+
+@when(u'we add a new entry with "{title}" and "{text}" as the title and text')
+def add(context, title, text):
+    context.page = context.client.post(
+        '/add',
+        data=dict(title=title, text=text),
+        follow_redirects=True
+    )
+    assert context.page
--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,6 @@
 # imports
 from flask import Flask, render_template, request, \
-    session, flash, redirect, url_for
+    session, flash, redirect, url_for, abort
 
 # configuration
 DATABASE = ''
@@ -38,6 +38,13 @@ def logout():
     """User logout/authentication/session management."""
     session.pop('logged_in', None)
     flash('You were logged out')
+    return redirect(url_for('index'))
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    flash('New entry was successfully posted')
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
